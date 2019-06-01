18679d08af1618f2eabe724913f3d97840f6949f
Gregory Simonian <simonian.7@buckeyemail.osu.edu>
2019-05-31 21:59:56 -0400

    Made a graph of just the data for a given state.
    This actualy first required futzing around with the DataFrame to make sure all
    the columns were read appropriately as numerical values instead of strings.

--- cdcdata.py
+++ cdcdata.py
@@ -10,13 +10,33 @@ CDC_FOLDER = Path.home() / "CDCData"
 def read_Diabetes_Data_file(datapath=CDC_FOLDER / "Diabetes_State.csv"):
     '''Read in a single file of Diabetes rates.
     
-    The file should be stored in CDC_FOLDER. This file should be taken from the
-    aggregated CDC data map.'''
+    The file shouuld be taken from the aggregated CDC Diabetes Atlas at:
+    
+    https://gis.cdc.gov/grasp/diabetes/DiabetesAtlas.html#
+
+    The file should be stored in CDC_FOLDER. This function wil return the
+    dataframe holding all of the data.'''
+    # Read the CSV.
     diabetes_data = pandas.read_csv(datapath, header=2)
+
+    # The last row of this file is just text. I want to drop it. However, it
+    # messes up ALL the datatypes, so we have to reset it.
     diabetes_data.drop(diabetes_data.tail(1).index, inplace=True)
-    diabetes_data["State"] = diabetes_data["State"].astype("category")
+
+    # This table has missing values, which are denoted by "No Value". This
+    # function remaps the "Number" column to be false where the "Number" is a
+    # string instead of a number.
     diabetes_data["Number"] = diabetes_data["Number"].where(
         diabetes_data["Number"].str.isnumeric())
+
+    # Converting the state column to be categorical makes it possible to get a
+    # list of states from the data to iterate through.
+    diabetes_data["State"] = diabetes_data["State"].astype("category")
+
+    # Convert the "Year" and "Number" columns to floats. They should 
+    # be integers, but integers can't hold a NaN value in pandas.
+    diabetes_data["Year"] = diabetes_data["Year"].astype(float)
+    diabetes_data["Number"] = diabetes_data["Number"].astype(float)
     return diabetes_data
 
 
--- linreg.py
+++ linreg.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 import cdcdata
+import matplotlib.pyplot as plt
 
 FULL_DATA = cdcdata.read_Diabetes_Data_file()
 
@@ -24,26 +25,54 @@ def predict_n(n, state):
     Predict_n will return a 2-tuple. The first element will be the predictions,
     an array of length n. The second element will be the mean square error of
     the validation set.'''
-    # If a row is missing data, ignore it?
-    # The next level is to impute it, but I don't believe in that.
-    state_data = FULL_DATA[np.logical_and(
-        FULL_DATA["State"] == state, ~pandas.isnull(FULL_DATA["Number"]))]
     last_year = int(max(state_data["Year"]))
     next_years = np.arange(n).reshape(-1, 1) + last_year
-    year_train, year_test, diabetes_train, diabetes_test = train_test_split(
-        state_data["Year"], state_data["Number"], test_size=0.3,
-        random_state=53019)
-    # Look more into Standardization here.
-    lr = LinearRegression(normalize=True)
-    year_train = year_train.values.reshape(-1, 1)
-    year_test = year_test.values.reshape(-1, 1)
-    diabetes_train = diabetes_train.values.reshape(-1, 1)
-    diabetes_test = diabetes_test.values.reshape(-1, 1)
-    lr.fit(year_train, diabetes_train)
     diabetes_pred = lr.predict(year_test)
     mean_square_error = mean_squared_error(diabetes_test, diabetes_pred)
     diabetes_new = lr.predict(next_years)
     return diabetes_new, mean_square_error
+
+def read_test_train_full(state, test_size=0.3):
+    '''Get a testing and training set from the full dataset.
+
+    This function reads the year and diabetes number from FULL_DATA for a
+    specific state, and then splits it into a testing and training set. The
+    fraction of testing points should be specified in test_size.
+    
+    This function will return a 4-tuple containing:
+    
+    (year_train, year_test, diabetes_train, diabetes_test)'''
+    # If a row is missing data, ignore it?
+    # The next level is to impute it, but I don't believe in that.
+    state_data = FULL_DATA[np.logical_and(
+        FULL_DATA["State"] == state, ~pandas.isnull(FULL_DATA["Number"]))]
+    # Split the data randomly into testing and training sets 70/30 and return
+    # the 4-tuple.
+    return train_test_split(
+        state_data["Year"], state_data["Number"], test_size=test_size,
+        random_state=53019)
+
+def full_linear_model(year_train, diabetes_train):
+    '''Train a linear model on the given diabetes dataset.
+
+    This model will be trained on all the data stored in FULL_DATA for the
+    specific state. It will return a LinearModel instance.'''
+
+    # The normalize=True keyword normalizes the X-dimension of the data. I
+    # don't think it really matters here, but other sources on data science
+    # note that you should do a process to scale the data so that it looks like
+    # a normal distribution, or a flat distribution. It may be important for
+    # more sophisticated analyses.
+    lr = LinearRegression(normalize=True)
+    # lr.fit requires 2-D arrays. The first dimension should be the samples (16
+    # in this case). The second dimension should be the number of predictors
+    # (1; just the year).
+    year_train = year_train.values.reshape(-1, 1)
+    diabetes_train = diabetes_train.values.reshape(-1, 1)
+    # Now this fits the training data to the linear model.
+    lr.fit(year_train, diabetes_train)
+    return lr
+
 
 def predict_states():
     '''Predict the next 3 years of diabetes in states.'''
@@ -56,5 +85,18 @@ def predict_states():
         errors[i] = mse
     return datatab
 
+def graph_state(state):
+    '''Graph the historical and predicted data for a given state.'''
+    # Get the testing and training data
+    year_train, year_test, diabetes_train, diabetes_test = read_test_train_full(
+        state, test_size=0.3)
+    # Get the lienar model for the trained data.
+    lr = full_linear_model(year_train, diabetes_train)
 
+    # Now rejoin to make the full dataset.
+    full_year = np.concatenate([year_train, year_test])
+    full_diabetes = np.concatenate([diabetes_train, diabetes_test])
+    print(full_year)
+    print(full_diabetes)
 
+    plt.plot(full_year, full_diabetes, 'bd')
